0b650b95cea25134bcd1cd9bc51d30faf68fb9d5
himanshups <74396707+himanshups@users.noreply.github.com>
2023-03-10 17:48:26 +0530

    Create Reverse the String.java

---
+++ Reverse the String.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+public class Main
+{
+	public static void main(String[] args) {
+		String s = "Hello";
+		// Method1 : String Builder Inbuilt Function 
+		StringBuilder str = new StringBuilder(s);
+		str=str.reverse();
+		System.out.println(str);
+		System.out.println("-------------------------------------------------------------------");
+		// Method2 : Logic 1 with O(n) Space and Time complexity
+		String fin = "";
+		for(int i = 0;i<s.length();i++){
+		    char ch = s.charAt(i);
+		    fin = ch+fin;
+		}
+		System.out.println(fin);
+		System.out.println("-------------------------------------------------------------------");
+        // Method 3 : Logic 2 with O(n/2) Time and O(1) Space complexity
+        char arr[] = s.toCharArray();
+        int start = 0,end = arr.length-1;
+        while(start<end){
+            char temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+        }
+        for(int i :arr)System.out.print(i);
+        // Method 4 : Logic 3 Using stack 
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0;i<s.length();i++) stack.push(s.charAt(i));
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop());
+        }
+        // End of the Program
+	}
+}
+
+/*
+Output of the Program
+
+olleH
+-------------------------------------------------------------------
+olleH
+-------------------------------------------------------------------
+olleH
+-------------------------------------------------------------------
+olleH
+
+
+
+*/
